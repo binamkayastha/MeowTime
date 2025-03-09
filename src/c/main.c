fc7929c501d713c983c04fdc4e22cdb21256956a
binamkayastha <binamkayastha@gmail.com>
2025-03-09 10:26:11 -0400

    Delete all start from exampel list

--- src/c/main.c
+++ src/c/main.c
@@ -1,90 +1,154 @@
-#include <pebble.h>
+#include "pebble.h"
 
-Window *window;
-TextLayer *text_layer_title;
-TextLayer *text_layer_middle;
-// -1 means the screen is on the main page
-int current_routine_index = -1;
-// TODO: Settings not implemented yet, requires multiple windows
-// char *main_top= "Click up to go to settings!";
-char *main_message = "Click select the next routine!";
-char *next_routine = "Morning Routine";
-// TODO: All routines not implemented yet, requires multiple windows
-// char *main_bottom= "Click down to see all routines!";
+#define NUM_MENU_SECTIONS 2
+#define NUM_MENU_ICONS 3
+#define NUM_FIRST_MENU_ITEMS 3
+#define NUM_SECOND_MENU_ITEMS 1
 
+static Window *s_main_window;
+static MenuLayer *s_menu_layer;
+static GBitmap *s_menu_icons[NUM_MENU_ICONS];
+static GBitmap *s_background_bitmap;
 
-char *routine[] = {
-  "Make Bed",
-  "Drink Water",
-  "Brush Teeth",
-};
+static int s_current_icon = 0;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  printf("Click handler %d\n", current_routine_index); // \n indicates a newline character
-  int len = sizeof(routine)/sizeof(routine[0]);
-  printf("sizeof %d\n", len); // \n indicates a newline character
-  // A single click has just occured
-  current_routine_index++;
-  if (current_routine_index == len) {
-    text_layer_set_text(text_layer_middle, "Done! Great Job");
-  } else if (current_routine_index >= len || current_routine_index == -1) {
-    text_layer_set_text(text_layer_middle, main_message);
-  } else {
-    text_layer_set_text(text_layer_middle, routine[current_routine_index]);
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return NUM_MENU_SECTIONS;
+}
+
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  switch (section_index) {
+    case 0:
+      return NUM_FIRST_MENU_ITEMS;
+    case 1:
+      return NUM_SECOND_MENU_ITEMS;
+    default:
+      return 0;
   }
 }
 
-static void click_config_provider(void *context) {
-  printf("Click config provider\n"); // \n indicates a newline character
-  // https://developer.rebble.io/developer.pebble.com/guides/events-and-services/buttons/index.html
-  // Subcribe to button click events here
-  ButtonId id = BUTTON_ID_SELECT;  // The Select button
-  // ButtonId id = BUTTON_ID_DOWN;       // The Down button
-  //     uint16_t repeat_interval_ms = 200;  // Fire every 200 ms while held down
-  // window_single_repeating_click_subscribe(id, repeat_interval_ms,
-                                                // down_repeating_click_handler);
-  window_single_click_subscribe(id, select_click_handler);
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
-void init() {
-  window = window_create();
-
-  // Add title
-  text_layer_title = text_layer_create(GRect(0, 0, 144, 40));
-  text_layer_set_text_alignment(text_layer_title, GTextAlignmentCenter);
-  text_layer_set_text(text_layer_title, "ROUTINII");
-  text_layer_set_font(text_layer_title, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  layer_add_child(window_get_root_layer(window),
-                    text_layer_get_layer(text_layer_title));
-
-
-  // Add middletext
-  text_layer_middle = text_layer_create(GRect(0, 70, 144, 40));
-  text_layer_set_text_alignment(text_layer_middle, GTextAlignmentCenter);
-  text_layer_set_text(text_layer_middle, routine[current_routine_index]);
-  text_layer_set_font(text_layer_middle, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-  layer_add_child(window_get_root_layer(window),
-                    text_layer_get_layer(text_layer_middle));
-
-
-  // Add button click providers
-  printf("Hello, World\n"); // \n indicates a newline character
-  window_set_click_config_provider(window, click_config_provider);
-
-
-
-  window_stack_push(window, true);
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  // Determine which section we're working with
+  switch (section_index) {
+    case 0:
+      // Draw title text in the section header
+      menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
+      break;
+    case 1:
+      menu_cell_basic_header_draw(ctx, cell_layer, "One more");
+      break;
+  }
 }
 
-void deinit() {
-  text_layer_destroy(text_layer_title);
-  text_layer_destroy(text_layer_middle);
-  window_destroy(window);
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  // Determine which section we're going to draw in
+  switch (cell_index->section) {
+    case 0:
+      // Use the row to specify which item we'll draw
+      switch (cell_index->row) {
+        case 0:
+          // This is a basic menu item with a title and subtitle
+          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
+          break;
+        case 1:
+          // This is a basic menu icon with a cycling icon
+          menu_cell_basic_draw(ctx, cell_layer, "Icon Item", "Select to cycle", NULL);
+          break;
+        // case 2: 
+        //   {
+        //     // Here we use the graphics context to draw something different
+        //     // In this case, we show a strip of a watchface's background
+        //     GSize size = layer_get_frame(cell_layer).size;
+        //     graphics_draw_bitmap_in_rect(ctx, s_background_bitmap, GRect(0, 0, size.w, size.h));
+        //   }
+        //   break;
+      }
+      break;
+    case 1:
+      switch (cell_index->row) {
+        case 0:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "Final Item");
+          break;
+      }
+  }
 }
 
-int main() {
+static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+  // Use the row to specify which item will receive the select action
+  switch (cell_index->row) {
+    // This is the menu item with the cycling icon
+    case 1:
+      // Cycle the icon
+      s_current_icon = (s_current_icon + 1) % NUM_MENU_ICONS;
+      // After changing the icon, mark the layer to have it updated
+      layer_mark_dirty(menu_layer_get_layer(menu_layer));
+      break;
+  }
+
+}
+
+static void main_window_load(Window *window) {
+  // Here we load the bitmap assets
+  s_menu_icons[0] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ONE);
+  s_menu_icons[1] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TWO);
+  s_menu_icons[2] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_THREE);
+
+  // And also load the background
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_BAR_ICON_CHECK);
+
+  // Now we prepare to initialize the menu layer
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Create the menu layer
+  s_menu_layer = menu_layer_create(bounds);
+  menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_sections = menu_get_num_sections_callback,
+    .get_num_rows = menu_get_num_rows_callback,
+    .get_header_height = menu_get_header_height_callback,
+    .draw_header = menu_draw_header_callback,
+    .draw_row = menu_draw_row_callback,
+    .select_click = menu_select_callback,
+  });
+
+  // Bind the menu layer's click config provider to the window for interactivity
+  menu_layer_set_click_config_onto_window(s_menu_layer, window);
+
+  layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy the menu layer
+  menu_layer_destroy(s_menu_layer);
+
+  // Cleanup the menu icons
+  for (int i = 0; i < NUM_MENU_ICONS; i++) {
+    gbitmap_destroy(s_menu_icons[i]);
+  }
+
+  gbitmap_destroy(s_background_bitmap);
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
   init();
   app_event_loop();
   deinit();
-  return 0;
 }
